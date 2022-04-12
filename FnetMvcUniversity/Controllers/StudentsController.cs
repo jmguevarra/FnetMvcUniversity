e3743221798ee1d3ad2cde4b94b0085ddb9ea7e2
jmguevarra <jaimeguevarra22@gmail.com>
2022-04-12 17:48:37 +1000

    Edit Action

--- FnetMvcUniversity/Controllers/StudentsController.cs
+++ FnetMvcUniversity/Controllers/StudentsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FnetMvcUniversity.Models;
+using System.Net;
+using System.Data.Entity;
 
 namespace FnetMvcUniversity.Controllers
 {
@@ -22,6 +24,39 @@ namespace FnetMvcUniversity.Controllers
         public ActionResult Index()
         {
             return View(db.Student.ToList()); ;
+        }
+
+        public ActionResult Edit(int? id) {
+
+            //return id is null
+            if ( id == null ) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            //check if id is exists in student record
+            Student student = db.Student.Find(id);
+            if ( student == null ) return HttpNotFound();
+
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id, StudLRN, FullName, FullName, StudCourse, StudYear, StudSection")] Student student) {
+
+            if (ModelState.IsValid) {
+
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        public ActionResult Delete()
+        {
+
+            return View();
         }
     }
 }
