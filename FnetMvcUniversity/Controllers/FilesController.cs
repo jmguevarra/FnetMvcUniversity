367bfb1cfde43bbbf2af23b8b1d8e3a4f20d080d
jmguevarra <jaimeguevarra22@gmail.com>
2022-04-13 17:12:01 +1000

    Retailer Participation with Partial View at Modal

---
+++ FnetMvcUniversity/Controllers/FilesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FnetMvcUniversity.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RetailerParticipationRepor() {
+            return PartialView();
+        }
+    }
+}
